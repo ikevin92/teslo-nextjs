3e370c4a04c4e272267fc13d062cbb8869a20ec2
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-16 23:33:32 -0500

    minicart empty

---
+++ pages/cart/empty.tsx
@@ -0,0 +1,41 @@
+import NextLink from 'next/link'
+import { RemoveShoppingCartOutlined } from '@mui/icons-material'
+import { Box, Link, Typography } from '@mui/material'
+import { ShopLayout } from '../../components/layouts'
+
+const EmptyPage = () => {
+  return (
+    <ShopLayout
+      title='Carrito vacio'
+      pageDescription='No hay articulos en el carrito de compras'
+    >
+      <Box
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
+        height='calc(100vh - 200px)'
+        sx={{
+          flexDirection: { xs: 'column', sm: 'row' },
+        }}
+      >
+        <RemoveShoppingCartOutlined sx={{ fontSize: 100 }} />
+        <Box display='flex' flexDirection={'column'} alignItems='center'>
+          <Typography
+          // variant='h1'
+          // component='h1'
+          // fontSize={80}
+          // fontWeight={200}
+          >
+            Su carrito está vacio
+          </Typography>
+          <NextLink href='/' passHref>
+            <Link typography='h4' color='secondary'>
+              Regresar
+            </Link>
+          </NextLink>
+        </Box>
+      </Box>
+    </ShopLayout>
+  )
+}
+export default EmptyPage
