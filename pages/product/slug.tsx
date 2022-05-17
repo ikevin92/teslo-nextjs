e0a39809f9fd92e109a351849a647d3edd65d42a
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-05-16 23:22:58 -0500

    selector de tallas

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,7 +1,8 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material'
 import { ShopLayout } from '../../components/layouts/ShopLayout'
 import { initialData } from '../../database/products'
-import { ProductSlideShow } from '../../components/products'
+import { ProductSlideShow, SizeSelector } from '../../components/products'
+import { ItemCounter } from '../../components/ui'
 
 const product = initialData.products[0]
 
@@ -19,13 +20,21 @@ const ProductPage = () => {
             <Typography variant='h1' component='h1'>
               {product.title}
             </Typography>
+
             <Typography variant='subtitle1' component='h2'>
               {`$${product.price}`}
             </Typography>
+
             {/* cantidad */}
             <Box sx={{ my: 2 }}>
               <Typography variant='subtitle2'>Cantidad</Typography>
+              <ItemCounter />
+              <SizeSelector
+                // selectedSize={product.sizes[0]}
+                sizes={product.sizes}
+              />
             </Box>
+
             {/* agregar al carrito */}
             <Button color='secondary' className='circular-btn'>
               Agregar al carrito
